74977f11987420ba9dca4ec62b3a676f423a023b
Daniel Collins <daniel.collins@usan.com>
2018-07-12 23:41:13 +0000

    Try some things for permissions
    doesn't work, though

--- telephony-bridge/entrypoint.sh
+++ telephony-bridge/entrypoint.sh
@@ -25,18 +25,26 @@ export AGENT_PORT=${AGENT_PORT:-4242}
 
 # Check for external cluster IP
 if [ "${EXTERNAL_IP}" = "" ] ; then
-        echo "ERROR: Cluster IP is not defined, please define external EXTERNAL_IP enviromental variable."
+        echo "ERROR: Load balancer name is not defined, please define external EXTERNAL_IP enviromental variable."
         echo "ERROR: Shutting down uDFE APP"
         sleep 5
         exit 1
 fi
+
+LOAD_BALANCER_NAME="${EXTERNAL_IP}"
+EXTERNAL_IP=$(curl curl --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://kubernetes/api/v1/namespaces/default/services/dialogflow-telephony-bridge-1-dfe-switch-svc 2>/dev/null | jq '.status.loadBalancer.ingress[0].ip')
+while [ ! -n "${EXTERNAL_IP}" ] ; do
+    printf "INFO: Waiting for external IP to be allocated to load balancer ${LOAD_BALANCER_NAME}\n"
+    sleep 5
+    EXTERNAL_IP=$(curl curl --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://kubernetes/api/v1/namespaces/default/services/dialogflow-telephony-bridge-1-dfe-switch-svc 2>/dev/null | jq '.status.loadBalancer.ingress[0].ip')
+done
 
 mkdir -p /etc/asterisk
 
 # Update asterisk conf files
 for f in etc_asterisk/* ; do
     if [ $(basename $f) != "extensions.conf" ] ; then
-        cat $f | envsubst > /etc/asterisk/$(basename $f)
+        cat $f | envsubst | tee /etc/asterisk/$(basename $f)
     else
         cp $f /etc/asterisk/$(basename $f)
     fi
